90214804e49f494ce8fad923bbd16944f896e6df
qiuruiyu <359603247@qq.com>
2021-10-09 05:55:02 +0800

    complete hw1

--- hw1/GuitarHeroLite.java
+++ hw1/GuitarHeroLite.java
@@ -1,5 +1,6 @@
 /** A client that uses the synthesizer package to replicate a plucked guitar string sound */
 public class GuitarHeroLite {
+    
     private static final double CONCERT_A = 440.0;
     private static final double CONCERT_C = CONCERT_A * Math.pow(2, 3.0 / 12.0);
 
